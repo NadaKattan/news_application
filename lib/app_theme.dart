e79f0795e50971973aeda160563bc639a4117a34
NadaKattan <nadakattan012@gmail.com>
2024-11-13 19:02:33 +0200

    add app theme

---
+++ lib/app_theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primary = Color(0xff39A552);
+  static const Color red = Color(0xffC91C22);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color black = Color(0xff303030);
+  static const Color grey = Color(0xff4F5A69);
+
+  static ThemeData lightThemeData = ThemeData(
+    primaryColor: primary,
+    textTheme: const TextTheme(
+      headlineSmall:
+          TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: white),
+      titleLarge:
+          TextStyle(fontSize: 22, fontWeight: FontWeight.w400, color: black),
+      labelMedium: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.bold,
+        color: black,
+      ),
+      bodyMedium: TextStyle(
+        fontSize: 13,
+        fontWeight: FontWeight.w400,
+        color: black,
+      ),
+    ),
+    scaffoldBackgroundColor: Colors.transparent,
+  );
+}
